b29b92cd2202b9adfb582494f50026940f47446f
JayJay1024 <lygm1062@gmail.com>
2023-04-23 21:49:12 +0800

    Fix api domain

--- .github/actions/checker/main.ts
+++ .github/actions/checker/main.ts
@@ -62,7 +62,7 @@ const tryIsValidSignatures = (signedMessages, signature, address) => {
 };
 
 const isValidAsset = async (id, symbol, network, owner, file) => {
-  const apiUrl = `https://${network}.webapi.subscan.io/api/scan/assets/asset`;
+  const apiUrl = `https://${network}.api.subscan.io/api/scan/assets/asset`;
   const body = JSON.stringify({ asset_id: id.toString() });
 
   const data = await nodeFetch(apiUrl, {
@@ -97,7 +97,7 @@ const isValidAsset = async (id, symbol, network, owner, file) => {
 };
 
 const isValidSystemCustom = async (symbol, category, network, file) => {
-  const apiUrl = `https://${network}.webapi.subscan.io/api/v2/scan/tokens`;
+  const apiUrl = `https://${network}.api.subscan.io/api/v2/scan/tokens`;
   const body = JSON.stringify({
     include_extends: true,
     page: 0,
@@ -134,7 +134,7 @@ const isValidSystemCustom = async (symbol, category, network, file) => {
 };
 
 const isValidERC20ERC721 = async (id, symbol, category, network, file) => {
-  const apiUrl = `https://${network}.webapi.subscan.io/api/scan/evm/tokens`;
+  const apiUrl = `https://${network}.api.subscan.io/api/scan/evm/tokens`;
   const body = JSON.stringify({
     contracts: [id.toString()],
   });
@@ -144,10 +144,6 @@ const isValidERC20ERC721 = async (id, symbol, category, network, file) => {
     headers,
     body,
   })
-    .then((res) => {
-      console.log('res', JSON.stringify(res));
-      return res;
-    })
     .then((resp) => resp.json())
     .then((data) => {
       if (data?.code === 0) {
